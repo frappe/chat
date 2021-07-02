609dc4957e60de99e344cc718ad9bdb2f1c72396
codescientist703 <pukimonlegend@gmail.com>
2021-07-02 16:10:51 +0530

    refactor: changed to dynamic button setup

--- chat/public/js/components/chat_bubble.js
+++ chat/public/js/components/chat_bubble.js
@@ -4,41 +4,43 @@ export default class ChatBubble {
 		this.setup();
 	}
 	setup() {
-		this.chat_bubble_open_html = `
-			<div class='chat-bubble p-3'
-				data-toggle="tooltip"
-				title="Chat with us"
-			>
-				<i class="fa fa-comment 
-					pl-2 fa-lg fa-flip-horizontal" 
-					aria-hidden="true">
-				</i>
-				<div>Chat With Us</div>
-			</div>
+		this.chat_bubble = document.createElement('div');
+		let classes = 'chat-bubble p-3';
+		let title = 'Chat with us';
+		let inner_html = `
+			<i class="fa fa-comment 
+			pl-2 fa-lg fa-flip-horizontal" 
+			aria-hidden="true">
+			</i>
+			<div>Chat With Us</div>
 		`;
-		this.chat_bubble_closed_html = `
-			<div class='chat-bubble chat-bubble-closed' 
-				data-toggle="tooltip"
-				title="Close Chat"
-			>
+		if (this.parent.is_open === false) {
+			classes = 'chat-bubble chat-bubble-closed';
+			title = 'Close Chat';
+			inner_html = `
 				<i class="fa fa-times fa-2x cross-icon" 
 					aria-hidden="true">
 				</i>
-			</div>
-		`;
+			`;
+		}
+		$(this.chat_bubble)
+			.addClass(classes)
+			.attr({
+				'data-toggle': 'tooltip',
+				title: title,
+			})
+			.html(inner_html);
+
 		this.render();
 		this.setup_events();
 	}
 	render() {
-		if (this.parent.is_open === true) {
-			this.parent.app_element.html(this.chat_bubble_open_html);
-		} else {
-			this.parent.app_element.html(this.chat_bubble_closed_html);
-		}
+		this.parent.app_element.append(this.chat_bubble);
 	}
 	setup_events() {
 		$('.chat-bubble').click(() => {
 			this.parent.is_open = !this.parent.is_open;
+			this.chat_bubble.remove();
 			this.setup();
 		});
 	}
--- chat/public/js/components/chat_list.js
+++ chat/public/js/components/chat_list.js
@@ -1,1 +1,10 @@
-	
+export default class ChatBubble {
+	constructor() {
+		this.setup();
+	}
+	setup() {
+		this.chat_list_container = `
+			<div class></div>
+		`;
+	}
+}
