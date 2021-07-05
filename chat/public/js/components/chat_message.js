f9df0c4102fad45bc18107eb2302f2a5ac9fc86d
codescientist703 <pukimonlegend@gmail.com>
2021-07-05 16:21:46 +0530

    feat: added html for whether msg is latest or not

--- chat/public/js/components/chat_message.js
+++ chat/public/js/components/chat_message.js
@@ -8,11 +8,18 @@ export default class ChatMessage {
 		this.$chat_message.addClass('chat-message');
 
 		const avatar_html = frappe.avatar(null, 'avatar-medium', 'Speedwagon');
+		this.is_latest = Math.random() < 0.5;
+
+		const is_latest_html = `
+			<div class="chat-latest"></div>
+		`;
 
 		const info_html = `
 			<div class="mr-auto chat-profile-info pl-3">
 				<div class="font-weight-bold">Nihal Mittal</div>
-				<div style="color: var(--gray-600)">Hey, Jotaro?</div>
+				<div style="color: ${
+					this.is_latest ? 'var(--gray-800)' : 'var(--gray-600)'
+				}">Hey, Jotaro?</div>
 			</div>
 		`;
 		const date_html = `
@@ -20,7 +27,12 @@ export default class ChatMessage {
 				Yesterday
 			</div>
 		`;
-		this.$chat_message.html(avatar_html + info_html + date_html);
+		let inner_html = '';
+		if (this.is_latest) {
+			inner_html += is_latest_html;
+		}
+		inner_html += avatar_html + info_html + date_html;
+		this.$chat_message.html(inner_html);
 		this.parent.$chat_message_container.append(this.$chat_message);
 	}
 }
---
+++ chat/public/js/components/chat_space.js
@@ -0,0 +1,5 @@
+export default class ChatSpace {
+	constructor(parent) {
+		this.parent = parent;
+	}
+}
