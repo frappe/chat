679bbe85e30934e63975f5035268a98b12c3560b
codescientist703 <pukimonlegend@gmail.com>
2021-07-08 12:57:39 +0530

    refactor: changed structure of chat list

--- chat/public/js/components/chat_list.js
+++ chat/public/js/components/chat_list.js
@@ -7,7 +7,13 @@ export default class ChatList {
 	}
 	setup() {
 		this.$chat_list = $(document.createElement('div'));
-		const classes = 'chat-list';
+		this.$chat_list.addClass('chat-list');
+		this.setup_header();
+		this.setup_search();
+		this.setup_message();
+		this.render();
+	}
+	setup_header() {
 		const chat_list_header_html = `
 			<div class="chat-list-header">
 				<h3>Chats</h3>
@@ -17,6 +23,9 @@ export default class ChatList {
 				</div>
 			</div>
 		`;
+		this.$chat_list.append(chat_list_header_html);
+	}
+	setup_search() {
 		const chat_list_search_html = `
 			<div class="input-group my-2 chat-search">
 				<i class="fa fa-search pt-2 pl-3"></i>
@@ -26,17 +35,16 @@ export default class ChatList {
 				id="chat-searh-bix">	
 			</div>
 		`;
-		const inner_html = chat_list_header_html + chat_list_search_html;
+		this.$chat_list.append(chat_list_search_html);
+	}
+	setup_message() {
 		this.$chat_message_container = $(document.createElement('div'));
 		this.$chat_message_container.addClass('chat-message-container');
 		this.chat_messages = [];
 		for (let i = 0; i < 20; i++) {
 			this.chat_messages.push(new ChatMessage(this));
 		}
-		this.$chat_list.addClass(classes).html(inner_html);
 		this.$chat_list.append(this.$chat_message_container);
-
-		this.render();
 	}
 	render() {
 		this.chat_messages.forEach((element) => {
