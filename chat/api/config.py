2eaf2ad726b1681451d0e00779fb5c9421868ba9
codescientist703 <pukimonlegend@gmail.com>
2021-07-14 13:21:17 +0530

    feat: added api method for getting settings

---
+++ chat/api/config.py
@@ -0,0 +1,10 @@
+import frappe
+from frappe import _
+
+
+@frappe.whitelist(allow_guest=True)
+def settings():
+    config = {
+        'socketio_port': frappe.conf.socketio_port
+    }
+    return config
