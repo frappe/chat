94fd3968f62d6e35eb04def7517bcb570bac3226
codescientist703 <pukimonlegend@gmail.com>
2021-07-15 17:39:26 +0530

    feat: added api method for fetching room info

--- chat/api/room.py
+++ chat/api/room.py
@@ -4,6 +4,15 @@ from frappe import _
 
 @frappe.whitelist(allow_guest=True)
 def get():
-    result = frappe.db.get_list(
-        'Chat Room', fields=['room_name', 'guest', 'last_message'])
-    return result
+    data = frappe.db.sql("""
+		SELECT
+			cr.name,
+			cr.modified,
+			cr.last_message,
+			cg.guest_name,
+			cg.email
+		FROM `tabChat Room` cr
+			LEFT JOIN `tabChat Guest` cg 
+			ON cr.guest = cg.name
+	""", as_dict=1)
+    return data
