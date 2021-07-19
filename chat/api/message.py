d228dafea838cf2038865c1dee1c08a025d6d0ee
codescientist703 <pukimonlegend@gmail.com>
2021-07-19 16:04:19 +0530

    feat: added room parameter in api message

--- chat/api/message.py
+++ chat/api/message.py
@@ -3,13 +3,13 @@ from frappe import _
 
 
 @frappe.whitelist(allow_guest=True)
-def send(message, user):
+def send(message, user, room):
     result = {
         'message': message,
         'user': user
     }
     frappe.publish_realtime(event='receive_message',
-                            message=result, room='test')
+                            message=result, room=room)
 
 
 @frappe.whitelist(allow_guest=True)
@@ -21,5 +21,4 @@ def get_all(room):
                                fields=['message', 'sender', 'creation'],
                                order_by='creation asc'
                                )
-    print(result)
     return result
