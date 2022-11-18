20ac75c1dcb548a319be43f3be1ba57f55d1a3bc
Gavin D'souza <gavin18d@gmail.com>
2022-11-18 18:43:35 +0530

    refactor: Message APIs
    - Use cached room information for publishing only to members
    - Add get_members helper method in chat room documents
    - Send typing data directly, defer other events post commit
    - Styled with black
    - Add typing hints for debuggability

--- chat/api/message.py
+++ chat/api/message.py
@@ -4,11 +4,11 @@ from chat.utils import update_room, is_user_allowed_in_room, raise_not_authorize
 
 
 @frappe.whitelist(allow_guest=True)
-def send(content, user, room, email):
+def send(content: str, user: str, room: str, email: str):
     """Send the message via socketio
 
     Args:
-        message (str): Message to be sent.
+        content (str): Message to be sent.
         user (str): Sender's name.
         room (str): Room's name.
         email (str): Sender's email.
@@ -16,41 +16,48 @@ def send(content, user, room, email):
     if not is_user_allowed_in_room(room, email, user):
         raise_not_authorized_error()
 
-    new_message = frappe.get_doc({
-        'doctype': 'Chat Message',
-        'content': content,
-        'sender': user,
-        'room': room,
-        'sender_email': email
-    }).insert()
+    new_message = frappe.get_doc(
+        {
+            "doctype": "Chat Message",
+            "content": content,
+            "sender": user,
+            "room": room,
+            "sender_email": email,
+        }
+    ).insert()
+
     update_room(room=room, last_message=content)
 
     result = {
-        'content': content,
-        'user': user,
-        'creation': new_message.creation,
-        'room': room,
-        'sender_email': email
+        "content": content,
+        "user": user,
+        "creation": new_message.creation,
+        "room": room,
+        "sender_email": email,
     }
-
     typing_data = {
-        'room': room,
-        'user': user,
-        'is_typing': 'false',
-        'is_guest': 'true' if user == 'Guest' else 'false',
+        "room": room,
+        "user": user,
+        "is_typing": "false",
+        "is_guest": "true" if user == "Guest" else "false",
     }
-    typing_event = room + ':typing'
+    typing_event = f"{room}:typing"
 
-    frappe.publish_realtime(event=room, message=result, after_commit=True)
-
-    frappe.publish_realtime(event='latest_chat_updates',
-                            message=result, after_commit=True)
-    frappe.publish_realtime(event=typing_event,
-                            message=typing_data, after_commit=True)
+    for chat_user in frappe.get_cached_doc("Chat Room", room).get_members():
+        frappe.publish_realtime(event=typing_event, user=chat_user, message=typing_data)
+        frappe.publish_realtime(
+            event=room, message=result, user=chat_user, after_commit=True
+        )
+        frappe.publish_realtime(
+            event="latest_chat_updates",
+            message=result,
+            user=chat_user,
+            after_commit=True,
+        )
 
 
 @frappe.whitelist(allow_guest=True)
-def get_all(room, email):
+def get_all(room: str, email: str):
     """Get all the messages of a particular room
 
     Args:
@@ -60,30 +67,30 @@ def get_all(room, email):
     if not is_user_allowed_in_room(room, email):
         raise_not_authorized_error()
 
-    result = frappe.db.get_all('Chat Message',
-                               filters={
-                                   'room': room,
-                               },
-                               fields=['content', 'sender',
-                                       'creation', 'sender_email'],
-                               order_by='creation asc'
-                               )
-    return result
+    return frappe.get_all(
+        "Chat Message",
+        filters={
+            "room": room,
+        },
+        fields=["content", "sender", "creation", "sender_email"],
+        order_by="creation asc",
+    )
 
 
 @frappe.whitelist()
-def mark_as_read(room):
+def mark_as_read(room: str):
     """Mark the message as read
 
     Args:
         room (str): Room's name.
     """
-    frappe.enqueue('chat.utils.update_room', room=room,
-                   is_read=1, update_modified=False)
+    frappe.enqueue(
+        "chat.utils.update_room", room=room, is_read=1, update_modified=False
+    )
 
 
 @frappe.whitelist(allow_guest=True)
-def set_typing(room, user, is_typing, is_guest):
+def set_typing(room: str, user: str, is_typing: bool, is_guest: bool):
     """Set the typing text accordingly
 
     Args:
@@ -92,12 +99,8 @@ def set_typing(room, user, is_typing, is_guest):
         is_typing (bool): Whether user is typing.
         is_guest (bool): Whether user is guest or not.
     """
-    result = {
-        'room': room,
-        'user': user,
-        'is_typing': is_typing,
-        'is_guest': is_guest
-    }
-    event = room + ':typing'
-    frappe.publish_realtime(event=event,
-                            message=result)
+    result = {"room": room, "user": user, "is_typing": is_typing, "is_guest": is_guest}
+    event = f"{room}:typing"
+
+    for chat_user in frappe.get_cached_doc("Chat Room", room).get_members():
+        frappe.publish_realtime(event=event, user=chat_user, message=result)
