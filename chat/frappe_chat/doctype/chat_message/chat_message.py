833624fd3de4ec257652e530221cac2774d64191
hendremy <hendriceremy@gmail.com>
2023-07-24 14:43:59 +0200

    Added automatic answer after sending message to AI Bot

--- chat/frappe_chat/doctype/chat_message/chat_message.py
+++ chat/frappe_chat/doctype/chat_message/chat_message.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2021, codescientist703 and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
 from frappe.model.document import Document
 
 class ChatMessage(Document):
-	pass
+	def on_update(self):
+		if not self.sender == 'AI':
+			content = "No" if len(self.content) == 5 else "Yes"
+			frappe.call('chat.api.message.send',content,'AI',self.room, 'ai@help.ai')
