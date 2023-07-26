375f39fb01c6df9bc0855774f87c0a6523adeaba
hendremy <hendriceremy@gmail.com>
2023-07-26 09:49:43 +0200

    Refactor chatbot reaction

--- chat/api/chatbot.py
+++ chat/api/chatbot.py
@@ -3,3 +3,18 @@ import frappe
 @frappe.whitelist()
 def get_email():
     return frappe.get_doc('Chat Settings').chatbot_email
+
+@frappe.whitelist()
+def chat(sender_email, room, content):
+    chat_settings = frappe.get_doc('Chat Settings')
+    frappe.call('chat.api.message.set_typing', room, chat_settings.chatbot_email, True, False)
+    try :
+        if chat_settings.chatbot_action:
+            answer = frappe.call(chat_settings.chatbot_action, sender_email, room, content)
+        else :
+            answer = 'Chatbot isn\'t linked to any method'
+    except:
+        answer = 'Something went wrong, try again later'
+    finally:
+        frappe.call('chat.api.message.set_typing', room, chat_settings.chatbot_email, False, False)
+        frappe.call('chat.api.message.send', answer, chat_settings.chatbot_email, room, chat_settings.chatbot_email)
--- chat/frappe_chat/doctype/chat_message/chat_message.py
+++ chat/frappe_chat/doctype/chat_message/chat_message.py
@@ -5,23 +5,15 @@ import frappe
 from frappe.model.document import Document
 
 class ChatMessage(Document):
+	
 	def on_update(self):
+		if self.message_is_for_chatbot():
+			frappe.call('chat.api.chatbot.chat', self.sender_email, self.room, self.content)
+
+	def message_is_for_chatbot(self):
 		chat_settings = frappe.get_doc('Chat Settings')
-		chatroom = frappe.get_doc('Chat Room', self.room)	
-
-		if chat_settings.enable_chatbot and chat_settings.chatbot_email in chatroom.members and not self.sender_email == chat_settings.chatbot_email:
-			frappe.call('chat.api.message.set_typing', self.room, chat_settings.chatbot_email, True, False)
-
-			try :
-				if chat_settings.chatbot_action:
-					answer = frappe.call(chat_settings.chatbot_action, self.sender_email, self.room, self.content)
-				else :
-					answer = 'Chatbot isn\'t linked to any method'
-			except:
-				answer = 'Something went wrong, try again later'
-			finally:
-				frappe.call('chat.api.message.set_typing', self.room, chat_settings.chatbot_email, False, False)
-				frappe.call('chat.api.message.send', answer, chat_settings.chatbot_email, self.room, chat_settings.chatbot_email)
-			
+		chatroom = frappe.get_doc('Chat Room', self.room)
+		return chat_settings.enable_chatbot and chat_settings.chatbot_email in chatroom.members and not self.sender_email == chat_settings.chatbot_email
+			 
 
 			
